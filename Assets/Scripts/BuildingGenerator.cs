fe1d36dbf565e804feb02875714844e2f27eb79c
Samuel Coleman <73917076+Samuel-Coleman-hub@users.noreply.github.com>
2023-03-12 17:16:38 +0000

    Implemented buildings scaling to different sizes

--- Assets/Scripts/BuildingGenerator.cs
+++ Assets/Scripts/BuildingGenerator.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BuildingGenerator : MonoBehaviour
@@ -28,12 +29,7 @@ public class BuildingGenerator : MonoBehaviour
 
                 if (gridMatrix[i, j].TileType.Equals(TileType.Empty) && UnityEngine.Random.value * gridMatrix[i, j].CenterScore <= randomPercentage * 3f && gridMatrix[i,j].NearRoad)
                 {
-                    Material mat = materials[UnityEngine.Random.Range(0, materials.Count)];
-                    //Randmoise scale
-
-                    int gridLength = gridMatrix.GetLength(0) > gridMatrix.GetLength(1) ? gridMatrix.GetLength(0) : gridMatrix.GetLength(1);
-                    float centerScorePercentage = (float)(gridMatrix[i, j].CenterScore / gridLength);
-                    float yScale = Mathf.Clamp(heightCurve.Evaluate(centerScorePercentage) * UnityEngine.Random.value * 5f, 0.5f, 10);
+                    
 
                     //Generate initial building
                     //GenerateBuilding(i, j, yScale, mat);
@@ -46,42 +42,61 @@ public class BuildingGenerator : MonoBehaviour
 
                     (emptyNeighbours, fullTiles, nextToRoad) = NumberOfEmptyNeighbours(i, j);
 
-                    int buildingTileSize = emptyNeighbours.Count;
-                    Debug.Log("Building tile size for " + i + " " + j  + ": is " + buildingTileSize);
+                    //JUST FOR DEBUGGING
+                    //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    //sphere.transform.position = centroid;
+
+                    Material mat = materials[UnityEngine.Random.Range(0, materials.Count)];
+                    //Randmoise scale
+                    Vector3 centroid = FindCentroid(emptyNeighbours);
+                    int gridLength = gridMatrix.GetLength(0) > gridMatrix.GetLength(1) ? gridMatrix.GetLength(0) : gridMatrix.GetLength(1);
+                    float centerScorePercentage = (float)(gridMatrix[i, j].CenterScore / gridLength);
+                    float yScale = Mathf.Clamp(heightCurve.Evaluate(centerScorePercentage) * UnityEngine.Random.value * 5f, 0.5f, 10);
+                    float xzScale = Mathf.Clamp(heightCurve.Evaluate(centerScorePercentage) * UnityEngine.Random.value * 5f, 1f, emptyNeighbours.Count * 0.35f);
+                    Debug.Log("xzScale " + xzScale);
+
+                    StartCoroutine(GenerateBuilding(centroid.x, centroid.z, yScale, xzScale, mat, emptyNeighbours.Count));
+
+
                     //(int)Math.Min(emptyNeighbours.Count, Math.Max(1, heightCurve.Evaluate(centerScorePercentage) * UnityEngine.Random.value * 5f));
-                    for(int tileIndex = 0; tileIndex < buildingTileSize; tileIndex++)
-                    {
-                        Debug.Log("Generating building " + emptyNeighbours[tileIndex].GetX() + " " + emptyNeighbours[tileIndex].GetY() + " for " + i + " " + j);
-                        StartCoroutine(GenerateBuilding(emptyNeighbours[tileIndex].GetX(), emptyNeighbours[tileIndex].GetY(), yScale, mat));
-                    }
+                    //for(int tileIndex = 0; tileIndex < buildingTileSize; tileIndex++)
+                    //{
+                    //    Debug.Log("Generating building " + emptyNeighbours[tileIndex].GetX() + " " + emptyNeighbours[tileIndex].GetY() + " for " + i + " " + j);
+                    //    StartCoroutine(GenerateBuilding(emptyNeighbours[tileIndex].GetX(), emptyNeighbours[tileIndex].GetY(), yScale, mat));
+                    //}
                 }
             }
         }
     }
 
-    private IEnumerator GenerateBuilding(int x, int z, float yScale, Material mat)
+    private IEnumerator GenerateBuilding(float x, float z, float yScale, float xzScale, Material mat, int numTiles)
     {
-        Debug.Log("In generateBuilding for " + x + " " + z);
-        GameObject tile = gridMatrix[x, z].Object;
+        GameObject buildingObject = Instantiate(buildingPrefab, new Vector3(x, 1f,
+            z), Quaternion.identity) as GameObject;
+        //Debug.Log("In generateBuilding for " + x + " " + z);
+        //GameObject tile = gridMatrix[x, z].Object;
 
-        Debug.Log("Before Object Instantiate " + x + " " + z);
-        GameObject buildingObject = Instantiate(buildingPrefab, new Vector3(tile.transform.position.x, tile.transform.position.y + 1f,
-            tile.transform.position.z), Quaternion.identity) as GameObject;
-        Debug.Log("after Object Instantiate " + x + " " + z);
-        buildingObject.name = "Building" + x + " " + z;
-        Debug.Log(buildingObject.name);
+        //Debug.Log("Before Object Instantiate " + x + " " + z);
+        //GameObject buildingObject = Instantiate(buildingPrefab, new Vector3(tile.transform.position.x, tile.transform.position.y + 1f,
+        //    tile.transform.position.z), Quaternion.identity) as GameObject;
+        //Debug.Log("after Object Instantiate " + x + " " + z);
+        //buildingObject.name = "Building" + x + " " + z;
+        //Debug.Log(buildingObject.name);
 
         yield return new WaitForSeconds(0f);
 
         buildingObject.GetComponent<MeshRenderer>().material = mat;
 
-        //Randomise scale of building
+        ////Randomise scale of building
         buildingObject.transform.position += Vector3.up * yScale / 2;
         buildingObject.transform.localScale += Vector3.up * yScale;
-        buildingObject.transform.parent = gridMatrix[x, z].Object.transform;
+        Vector3 scale = buildingObject.transform.localScale;
+        //buildingObject.transform.localScale = new Vector3(scale.x * numTiles * 0.45f, scale.y, scale.z * numTiles * 0.45f);
+        buildingObject.transform.localScale = new Vector3(xzScale, scale.y, xzScale);
+        //buildingObject.transform.parent = gridMatrix[x, z].Object.transform;
 
-        gridMatrix[x, z].ChildObject = buildingObject;
-        gridMatrix[x, z].TileType = TileType.Building;
+        //gridMatrix[x, z].ChildObject = buildingObject;
+        //gridMatrix[x, z].TileType = TileType.Building;
     }
 
     private (List<GridTile>,int,bool) NumberOfEmptyNeighbours(int x, int z)
@@ -98,97 +113,111 @@ public class BuildingGenerator : MonoBehaviour
         emptyNeighbours.Add(gridMatrix[x, z]);
         //GridTile[,] emptyTiles = new GridTile[1, 2];
         //emptyTiles[1, 1] = gridMatrix[x, z];
-
         Debug.Log("This is for x: " + x + " z: " + z);
+        List<int> numBuildingsPerRow = new List<int>();
         for(int i = minX; i <= maxX; i++)
         {
+            int zCount = 0;
             for (int j = minZ; j <= maxZ; j++)
             {
                 Debug.Log("IS A Neighbour of " + x + " " + z + " is: " + i + " " + j);
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.position = new Vector3(i, 0, j);
+                //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //cube.transform.position = new Vector3(i, 0, j);
                 if (gridMatrix[i, j].TileType.Equals(TileType.Empty))
                 {
+                    emptyNeighbours.Add(gridMatrix[i, j]);
+                    gridMatrix[i, j].TileType = TileType.Building;
+                    zCount++;
                     //Here need to work out if its next to another cell already in the emptyNeighbours list
                     //If it is then add to list if not then clear list
-                    Debug.Log("Square check incoming for neighbouring cell " + i + " " + j);
-                    if (emptyNeighbours.Count <= 1)
-                    {
-                        Debug.Log("Auto add " + i + " " + j);
-                        emptyNeighbours.Add(gridMatrix[i, j]);
-                        gridMatrix[i, j].TileType = TileType.Building;
-                    }
-                    else
-                    {
-                        emptyNeighbours.Add(gridMatrix[i, j]);
+                    //Debug.Log("Square check incoming for neighbouring cell " + i + " " + j);
+                    //if (emptyNeighbours.Count <= 1)
+                    //{
+                    //    Debug.Log("Auto add " + i + " " + j);
+                    //    emptyNeighbours.Add(gridMatrix[i, j]);
+                    //    gridMatrix[i, j].TileType = TileType.Building;
+                    //}
+                    //else
+                    //{
+                    //    emptyNeighbours.Add(gridMatrix[i, j]);
 
-                        //This method works for now but should not be the final solution
-                        gridMatrix[i, j].TileType = TileType.Building;
-                        //Debug.Log("Should I look left?");
-                        ////Left
-                        //if (j - 1 >= 0 && j - 1 < gridMatrix.GetLength(1))
-                        //{
-                        //    Debug.Log(i + " " + j + " looking left");
-                        //    if (emptyNeighbours.Contains(gridMatrix[i, j - 1]))
-                        //    {
-                        //        Debug.Log(i + " " + j + " left yes");
-                        //        emptyNeighbours.Add(gridMatrix[i, j]);
-                        //        //break;
-                        //    }
-                        //}
+                    //    //This method works for now but should not be the final solution
+                    //    gridMatrix[i, j].TileType = TileType.Building;
+                    //    //Debug.Log("Should I look left?");
+                    //    ////Left
+                    //    //if (j - 1 >= 0 && j - 1 < gridMatrix.GetLength(1))
+                    //    //{
+                    //    //    Debug.Log(i + " " + j + " looking left");
+                    //    //    if (emptyNeighbours.Contains(gridMatrix[i, j - 1]))
+                    //    //    {
+                    //    //        Debug.Log(i + " " + j + " left yes");
+                    //    //        emptyNeighbours.Add(gridMatrix[i, j]);
+                    //    //        //break;
+                    //    //    }
+                    //    //}
 
-                        //Debug.Log("Should I look right?");
-                        ////right
-                        //if (j + 1 >= 0 && j + 1 < gridMatrix.GetLength(1))
-                        //{
-                        //    Debug.Log(i + " " + j + " looking right");
-                        //    if (emptyNeighbours.Contains(gridMatrix[i, j + 1]))
-                        //    {
-                        //        Debug.Log(i + " " + j + " right yes");
-                        //        emptyNeighbours.Add(gridMatrix[i, j]);
-                        //       // break;
-                        //    }
-                        //}
+                    //    //Debug.Log("Should I look right?");
+                    //    ////right
+                    //    //if (j + 1 >= 0 && j + 1 < gridMatrix.GetLength(1))
+                    //    //{
+                    //    //    Debug.Log(i + " " + j + " looking right");
+                    //    //    if (emptyNeighbours.Contains(gridMatrix[i, j + 1]))
+                    //    //    {
+                    //    //        Debug.Log(i + " " + j + " right yes");
+                    //    //        emptyNeighbours.Add(gridMatrix[i, j]);
+                    //    //       // break;
+                    //    //    }
+                    //    //}
 
-                        //Debug.Log("Should I look up?");
-                        ////up
-                        //if (i - 1 >= 0 && i - 1 < gridMatrix.GetLength(0))
-                        //{
-                        //    Debug.Log(i + " " + j + " looking up");
-                        //    if (emptyNeighbours.Contains(gridMatrix[i - 1, j]))
-                        //    {
-                        //        Debug.Log(i + " " + j + " up yes");
-                        //        emptyNeighbours.Add(gridMatrix[i, j]);
-                        //       // break;
-                        //    }
-                        //}
+                    //    //Debug.Log("Should I look up?");
+                    //    ////up
+                    //    //if (i - 1 >= 0 && i - 1 < gridMatrix.GetLength(0))
+                    //    //{
+                    //    //    Debug.Log(i + " " + j + " looking up");
+                    //    //    if (emptyNeighbours.Contains(gridMatrix[i - 1, j]))
+                    //    //    {
+                    //    //        Debug.Log(i + " " + j + " up yes");
+                    //    //        emptyNeighbours.Add(gridMatrix[i, j]);
+                    //    //       // break;
+                    //    //    }
+                    //    //}
 
-                        //Debug.Log("Should I look down?");
-                        ////down
-                        //if (i + 1 >= 0 && i + 1 < gridMatrix.GetLength(0))
-                        //{
-                        //    Debug.Log(i + " " + j + " looking down");
-                        //    if (emptyNeighbours.Contains(gridMatrix[i + 1, j]))
-                        //    {
-                        //        Debug.Log(i + " " + j + " down yes");
-                        //        emptyNeighbours.Add(gridMatrix[i, j]);
-                        //       // break;
-                        //    }
-                        //}
+                    //    //Debug.Log("Should I look down?");
+                    //    ////down
+                    //    //if (i + 1 >= 0 && i + 1 < gridMatrix.GetLength(0))
+                    //    //{
+                    //    //    Debug.Log(i + " " + j + " looking down");
+                    //    //    if (emptyNeighbours.Contains(gridMatrix[i + 1, j]))
+                    //    //    {
+                    //    //        Debug.Log(i + " " + j + " down yes");
+                    //    //        emptyNeighbours.Add(gridMatrix[i, j]);
+                    //    //       // break;
+                    //    //    }
+                    //    //}
 
-                        //Debug.Log(i + " " + j + " is not in a square");
-                        //emptyNeighbours.Clear();
-                        //emptyNeighbours.Add(gridMatrix[x, z]);
-                    }
+                    //    //Debug.Log(i + " " + j + " is not in a square");
+                    //    //emptyNeighbours.Clear();
+                    //    //emptyNeighbours.Add(gridMatrix[x, z]);
+                    //}
                     Debug.Log("End of search for " + i + " " + j);
-
                 }
-
             }
+            numBuildingsPerRow.Add(zCount);
         }
 
         Debug.Log("For " + x + " " + z + " "+ emptyNeighbours.Count + "  and distinct" + emptyNeighbours.Distinct().ToList().Count);
         emptyNeighbours = emptyNeighbours.Distinct().ToList();
+
+        if(emptyNeighbours.Count == 3 || emptyNeighbours.Count == 5 || emptyNeighbours.Count == 7)
+        {
+            Debug.Log("There is a problem at " + x + " " + z + " because count is " + emptyNeighbours.Count);
+            int unique = numBuildingsPerRow.Distinct().First();
+            if (unique == 1)
+            {
+                Debug.Log("It is one in column " + numBuildingsPerRow.IndexOf(unique));
+            }
+        }
+
         return (emptyNeighbours, fullTiles, nextToRoad);
         ////I could make this a four loop
         //int emptyNeighbours = 4;
@@ -233,6 +262,21 @@ public class BuildingGenerator : MonoBehaviour
 
         //return (allEmpty, emptyNeighbours);
 
+    }
+
+    public Vector3 FindCentroid(List<GridTile> neighbouringTiles)
+    {
+        Vector3 centroid = new Vector3(0,0,0);
+        int numPoints = neighbouringTiles.Count;
+
+        foreach(GridTile tile in neighbouringTiles) 
+        {
+            centroid += tile.Object.transform.position;
+        }
+
+        centroid /= numPoints;
+
+        return centroid;
     }
 
     public void GenerateBuildings(List<Material> materials, GameObject buildingPrefab, float randomPercentage, GridTile[,] gridMatrix)
